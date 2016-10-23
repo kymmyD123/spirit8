c82cfb9f296f295c48050426bc76690a32b30fb6
Valera <kymmyd123@gmail.com>
2016-10-23 16:14:11 +0300

    Added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+    // Add a slide
+    $('.slick-js').slick('slickAdd',"<div></div>");
+
+// Get the current slide
+    var currentSlide = $('.slick-js').slick('slickCurrentSlide');
+});
