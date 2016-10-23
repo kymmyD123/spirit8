bb53f18b777bd9d8ea42daa1c1b5a769d738497f
Valera <kymmyd123@gmail.com>
2016-10-23 22:08:27 +0300

    ADDED

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $('.slick-js').slick({
-        setting-name: setting-value
-});
+        slidesToScroll: 4,
+        slidesToShow: 4,
+        arrows: false,
+        dots: true
+    });
 });
